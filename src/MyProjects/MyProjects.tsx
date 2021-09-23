091e18002d848f14f1521622993639fbfa266fe4
ellijahcuts <ellijahcuts@gmail.com>
2021-09-23 23:52:06 +0300

    update img

--- src/MyProjects/MyProjects.tsx
+++ src/MyProjects/MyProjects.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import style from './MyProjects.module.css';
-import styleContainer from "../common/styles/Container.module.css";
+import styleContainer from '../common/styles/Container.module.css';
 import Project from "./Project/Project";
 import scImg from '../common/images/sc.png'
 import todoImg from '../common/images/todo.png'
 import counterImg from '../common/images/counter.png'
-import Title from "../common/components/title/Title";
-
+import Title from '../common/components/title/Title';
 
 const MyProjects = () => {
-    return (
+
+    const social = {
+        backgroundImage: `url(${scImg})`
+    }
+    const todo = {
+        backgroundImage: `url(${todoImg})`
+    }
+    const counter = {
+        backgroundImage: `url(${counterImg})`
+    }
+
+        return (
         <div className={style.projectsCore}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <Title title={"My projects"}/>
                 <div className={style.projects}>
                     <Project title={"Social Network"}
-                             img={scImg}
+                             style={social}
                              description={"онлайн-платформа, которая используется для общения, знакомств, создания социальных отношений между людьми, которые имеют схожие интересы или офлайн-связи, а также для развлечения (музыка, фильмы) и работы."}
                     />
                     <Project title={"ToDoList"}
-                             img={todoImg}
+                             style={todo}
                              description={"Это простой список задач. Нужен он для того чтобы контролировать и не забывать дела, которые нужно сделать за день."}/>
                     <Project title={"Click Counter"}
-                             img={counterImg}
+                             style={counter}
                              description={"Простой счетчик кликов с настройками параметров, задание для IT-incubator'a"}/>
                 </div>
             </div>
--- src/MyProjects/Project/Project.tsx
+++ src/MyProjects/Project/Project.tsx
@@ -3,23 +3,20 @@ import style from "../../MyProjects/Project/Project.module.css";
 
 export type ProjectPropsType = {
     title: string
-    img: string
+    style: { backgroundImage: string; }
     description: string
 }
 
 const Project = (props: ProjectPropsType) => {
     return (
         <div className={style.projectCore}>
-            <div className={style.iconContainer}>
-                <img src={props.img} className={style.icon}/>
+            <div className={style.projectImg} style={props.style}>
+                <button className={style.button}>Смотреть</button>
             </div>
-            <button className={style.button}>Смотреть</button>
-            <div className={style.projectInfo}>
             <span className={style.description}>
                 <h4 className={style.projectTitle}>{props.title}</h4>
                 {props.description}
             </span>
-            </div>
         </div>
     );
 };
