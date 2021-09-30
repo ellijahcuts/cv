6b485ae865fbb9e9a9acbde8a91e0a3a2832d058
ellijahcuts <ellijahcuts@gmail.com>
2021-09-30 03:13:50 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import Footer from "./Footer/Footer";
 function App() {
     return (
         <div className="App">
-            <Header/>
             <Main/>
             <Skills/>
             <MyProjects/>
--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -4,19 +4,26 @@ import styleContainer from './../common/styles/Container.module.css';
 import myPhoto from '../common/images/my-photo.png'
 
 
+
 const Main = () => {
     return (
         <div className={style.mainBlock}>
-            <div className={styleContainer.container}>
+            <div className={styleContainer.containerForMain}>
                 <div className={style.text}>
                     <span>Hey</span>
                     <h1>I am Ilya Kazak</h1>
                     <p>Frontend Developer</p>
                 </div>
                 <div className={style.photoContainer}>
-                    <img className={style.photo} src={myPhoto}/>
+                    <img className={style.photo} src={myPhoto} alt={'Sorry this bug :)'}/>
                 </div>
             </div>
+            <div className={style.videoBg}>
+            <video className={style.video} preload={"auto"} muted autoPlay loop>
+                <source src={"https://cdn.videvo.net/videvo_files/video/free/2012-07/large_watermarked/FreeSmokeYoutube_preview.mp4"} type="video/mp4"/>
+                Your browser does not support the video tag.
+            </video>
+        </div>
         </div>
     );
 };
