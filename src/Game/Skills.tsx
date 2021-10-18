a411b9731511684a76a6af285c2417b14695c0ed
ellijahcuts <ellijahcuts@gmail.com>
2021-10-18 20:05:43 +0300

    update and add App Alt

---
+++ src/Game/Skills.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Skills.module.scss';
+
+
+const Skills = () => {
+    return (
+        <div className={style.skillBlock}>
+        </div>
+    );
+};
+
+export default Skills;
