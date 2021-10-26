15e66efdb8cb697643a7064d923da2b4feff787a
ellijahcuts <ellijahcuts@gmail.com>
2021-10-26 17:36:35 +0300

    upgrade

--- src/Main/MainAlt.tsx
+++ src/Main/MainAlt.tsx
@@ -10,7 +10,7 @@ import {Fade} from "react-awesome-reveal";
 const particlesOptions = {
     particles: {
         number: {
-            value: 110
+            value: 60
         },
         move: {
             speed: 0.40
@@ -32,18 +32,15 @@ const MainAlt = () => {
         <div className={style.mainBlock}>
             <Particles className={style.particles} params={particlesOptions}/>
             <div className={styleContainer.container}>
-                <Fade cascade>
                     <div className={style.textBlock}>
                         <span>Hello my friend</span>
                         <h1>I am Ilya Kazak</h1>
-                        <ReactTypingEffect className={style.readingText} speed={50} eraseSpeed={50}
-                                           text={["Frontend Developer " +
-                                           "| React, TypeScript"]}/>
+                        <ReactTypingEffect className={style.readingText} speed={30} eraseSpeed={30}
+                                           text={["Frontend Developer.","React, TypeScript."]}/>
                     </div>
                     <div className={style.photoContainer}>
                         <img className={style.photo} src={myPhoto}/>
                     </div>
-                </Fade>
             </div>
         </div>
     );
